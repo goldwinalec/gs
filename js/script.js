5cb6fa3976c241937cdb6a937f2aed5560b26676
Alina Gladkova <a-g1997@yandex.ru>
2021-12-02 18:42:37 +0500

    Стилизован слайдер steps

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  lazy: true,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});

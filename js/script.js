79ece2d8bbe1a3906e647d256e7327778ef7f535
Alina Gladkova <a-g1997@yandex.ru>
2021-12-03 19:46:21 +0500

    Обновлен блок check и добавлены всплывающие окна

--- js/script.js
+++ js/script.js
@@ -35,3 +35,74 @@ const feedbackSwiper = new Swiper('.feedback__swiper', {
 new Accordion('.faq__items', {
   openOnInit: [2],
 });
+
+const html = document.querySelector('html');
+const overlay = document.querySelector('.overlay');
+const btnsOpenLogin = document.querySelectorAll('#login');
+const btnsOpenRegister = document.querySelectorAll('#register');
+const btnOpenFeedback = document.querySelector('#feedback-btn');
+const btnOpenPassword = document.querySelector('#password');
+const btnsClose = document.querySelectorAll('.close__btn');
+const modalWrapper = document.querySelector('.modal');
+const modalLogin = document.querySelector('.modal__login');
+const modalRegister = document.querySelector('.modal__register');
+const modalFeedback = document.querySelector('.modal__feedback');
+const modalPassword = document.querySelector('.modal__password');
+
+const closeModal = function () {
+  modalWrapper.classList.remove('modal--visible');
+  const modals = [...modalWrapper.children];
+  modals.forEach((modal) => {
+    modal.classList.remove('modal--visible');
+  });
+  html.classList.remove('page--disabled');
+  overlay.classList.remove('overlay--visible');
+};
+
+const openModal = function () {
+  overlay.classList.add('overlay--visible');
+  modalWrapper.classList.add('modal--visible');
+  html.classList.add('page--disabled');
+};
+
+btnsOpenLogin.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeModal();
+    openModal();
+    modalLogin.classList.add('modal--visible');
+  });
+});
+
+btnsOpenRegister.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    openModal();
+    modalRegister.classList.add('modal--visible');
+  });
+});
+
+btnOpenPassword.addEventListener('click', function (e) {
+  e.preventDefault();
+  closeModal();
+  openModal();
+  modalPassword.classList.add('modal--visible');
+});
+
+btnOpenFeedback.addEventListener('click', function (e) {
+  e.preventDefault();
+  openModal();
+  modalFeedback.classList.add('modal--visible');
+});
+
+overlay.addEventListener('click', function (e) {
+  e.preventDefault();
+  closeModal();
+});
+
+btnsClose.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeModal();
+  });
+});

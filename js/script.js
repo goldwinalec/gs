1a1cb7ffe54007e6a0c97a8c8cced4e1b74f08ee
Alina Gladkova <a-g1997@yandex.ru>
2021-12-06 13:47:12 +0500

    Добавлена возможность посмотреть пароль и обновлена форма поиска аккаунта

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+////////////////////////////////////////////
+// Slider
+
 const swiper = new Swiper('.swiper', {
   lazy: true,
   loop: true,
@@ -32,9 +35,15 @@ const feedbackSwiper = new Swiper('.feedback__swiper', {
   },
 });
 
+////////////////////////////////////////////
+// FAQ section
+
 new Accordion('.faq__items', {
   openOnInit: [2],
 });
+
+////////////////////////////////////////////
+// Modal windows
 
 const html = document.querySelector('html');
 const overlay = document.querySelector('.overlay');
@@ -43,6 +52,9 @@ const btnsOpenRegister = document.querySelectorAll('#register');
 const btnOpenFeedback = document.querySelector('#feedback-btn');
 const btnOpenPassword = document.querySelector('#password');
 const btnsClose = document.querySelectorAll('.close__btn');
+const btnsSeePass = document.querySelectorAll('.see-password');
+const btnsHidePass = document.querySelectorAll('.hide-password');
+const inputsPass = document.querySelectorAll('.form__input--password');
 const modalWrapper = document.querySelector('.modal');
 const modalLogin = document.querySelector('.modal__login');
 const modalRegister = document.querySelector('.modal__register');
@@ -57,6 +69,9 @@ const closeModal = function () {
   });
   html.classList.remove('page--disabled');
   overlay.classList.remove('overlay--visible');
+  inputsPass.forEach((input) => input.setAttribute('type', 'password'));
+  btnsSeePass.forEach((btnHide) => btnHide.classList.remove('hidden'));
+  btnsHidePass.forEach((btnHide) => btnHide.classList.add('hidden'));
 };
 
 const openModal = function () {
@@ -105,4 +120,140 @@ btnsClose.forEach((btn) => {
     e.preventDefault();
     closeModal();
   });
+});
+
+btnsHidePass.forEach((btnHide) => btnHide.classList.add('hidden'));
+
+btnsSeePass.forEach((btn) => {
+  btn.addEventListener('click', function () {
+    inputsPass.forEach((input) => input.setAttribute('type', 'text'));
+    btn.classList.add('hidden');
+    btnsHidePass.forEach((btnHide) => btnHide.classList.remove('hidden'));
+  });
+  btnsHidePass.forEach((btn) => {
+    btn.addEventListener('click', function () {
+      inputsPass.forEach((input) => input.setAttribute('type', 'password'));
+      btn.classList.add('hidden');
+      btnsSeePass.forEach((btnHide) => btnHide.classList.remove('hidden'));
+    });
+  });
+});
+
+////////////////////////////////////////////
+// Check Instagram account
+const checkForm = document.querySelector('.check__form');
+const checkInput = document.querySelector('.check__input');
+const checkLabel = document.querySelector('.check__label');
+const tagsWrapper = document.querySelector('.tags__list');
+const items = [...document.querySelectorAll('.found__item')];
+const resultWrapper = document.querySelector('.check__results-wrapper');
+const error = document.querySelector('.check__result--error');
+const searching = document.querySelector('.check__result--searching');
+const found = document.querySelector('.check__result--found');
+const gettingData = document.querySelector('.check__process');
+const doneSearching = document.querySelector('.check__inner--done');
+const btnsChoose = document.querySelectorAll('.found__btn');
+const btnStart = document.querySelector('.check__info-btn');
+const checkBtn = document.querySelector('.check__btn');
+
+const hideResults = function () {
+  searching.classList.remove('check__result--visible');
+  found.classList.remove('check__result--visible');
+  error.classList.remove('check__result--visible');
+  resultWrapper.classList.remove('check__results-wrapper--visible');
+  checkBtn.disabled = true;
+};
+
+checkForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+});
+
+checkInput.addEventListener('keyup', function (e) {
+  hideResults();
+  if (checkInput.value.length > 2) {
+    searching.classList.add('check__result--visible');
+    resultWrapper.classList.add('check__results-wrapper--visible');
+    // setTimeout(function () {
+    //   error.classList.add('check__result--visible');
+    //   searching.classList.remove('check__result--visible');
+    // }, 1000);
+
+    setTimeout(function () {
+      found.classList.add('check__result--visible');
+      searching.classList.remove('check__result--visible');
+
+      btnsChoose.forEach((btn) => {
+        const onMouseOut = function () {
+          btn.textContent = 'Добавлен';
+        };
+
+        const onMouseOver = function () {
+          btn.textContent = 'Удалить';
+          btn.addEventListener('mouseout', onMouseOut);
+        };
+
+        const deleteAccount = function () {
+          btn.parentElement.classList.remove('found__item--add');
+          btn.textContent = 'Выбрать';
+          if (!tagsWrapper.firstChild) checkBtn.disabled = true;
+
+          btn.removeEventListener('mouseenter', onMouseOver);
+          btn.removeEventListener('mouseout', onMouseOut);
+          btn.removeEventListener('click', deleteAccount);
+        };
+
+        btn.addEventListener('click', function (e) {
+          e.preventDefault();
+          btn.parentElement.classList.add('found__item--add');
+
+          if (btn.parentElement.classList.contains('found__item--add')) {
+            const id = (Date.now() + '').slice(-10);
+            console.log(id);
+            const name =
+              btn.previousSibling.previousSibling.innerText.split('\n');
+            const html = `<li class="tags__item id="${id}">${name[0]}<span id="${id}" class="tags__btn"><img src="/images/close-tag.svg" alt=""></span></li>`;
+            btn.id = id;
+            tagsWrapper.insertAdjacentHTML('beforeEnd', html);
+
+            btn.textContent = 'Добавлен';
+            btn.addEventListener('mouseenter', onMouseOver);
+
+            const deleteTagBtns = [...document.querySelectorAll('.tags__btn')];
+            deleteTagBtns.forEach((tag) => {
+              tag.addEventListener('click', function () {
+                tag.parentElement.remove();
+                if (!tagsWrapper.firstChild) {
+                  hideResults();
+                  items.forEach((item) => {
+                    item.classList.remove('found__item--add');
+                  });
+                  deleteAccount();
+                }
+                btnsChoose.forEach((btn) => {
+                  btn.textContent = 'Выбрать';
+                });
+              });
+            });
+            checkInput.value = '';
+            checkInput.placeholder = '';
+            if (tagsWrapper.firstChild) checkBtn.disabled = false;
+            if (!tagsWrapper.firstChild) {
+              hideResults();
+              items.forEach((item) => {
+                item.classList.remove('found__item--add');
+              });
+              deleteAccount();
+            }
+            btn.addEventListener('click', deleteAccount);
+          } else {
+            btn.textContent = 'Выбрать';
+            btn.removeEventListener('mouseenter', onMouseOver);
+            btn.removeEventListener('mouseout', onMouseOut);
+          }
+        });
+      });
+    }, 1000);
+  } else {
+    hideResults();
+  }
 });
